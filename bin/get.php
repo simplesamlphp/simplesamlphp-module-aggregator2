8b595303c2f14239a5c38cc9b695607d952d197f
Tim van Dijen <tvdijen@gmail.com>
2023-01-04 16:52:58 +0100

    Fix concatenated calls to dirname

--- bin/get.php
+++ bin/get.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/lib/_autoload.php');
+require_once(dirname(__FILE__, 4) . '/lib/_autoload.php');
 
 if ($argc < 2) {
     fwrite(STDERR, "Missing aggregator id.\n");
